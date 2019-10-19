596fa347cbfb1bc660382830f275c5f3d2bebbec
Utkarsh <utkarshdeep@outlok.com>
2019-10-20 02:02:19 +0530

    app creation revamped

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import jwt from 'express-jwt'
 import { Routes } from './routes'
-import { connectMongo, errorHandler, verifyToken } from './config'
+import { connectMongo, errorHandler, JWT_CONFIG, CORS_OPTIONS } from './config'
+import { decryptMiddleware } from './utils'
 
-export const initApp = async () => {
-  // Initiate express
-  const app = express()
+// Initiate express
+const app = express()
 
-  app.use(cors({
-    origin: process.env.FRONT_END_URL,
-    optionsSuccessStatus: 200
-  }))
-  // Parse incoming request body
-  app.use(bodyParser.json({ limit: '5mb' }))
-  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true, parameterLimit: 50 }))
+app.use(cors(CORS_OPTIONS))
+// Parse incoming request body
+app.use(bodyParser.json({ limit: '5mb' }))
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true, parameterLimit: 50 }))
+app.use(decryptMiddleware)
+app.use(jwt({ secret: JWT_CONFIG.SECERET }).unless({ path: JWT_CONFIG.NO_AUTH_PATHS }))
 
-  app.use(verifyToken)
+// Create MongoDB connection
+connectMongo()
 
-  // Create MongoDB connection
-  await connectMongo()
+// Initiate all routes
+Routes(app)
 
-  // Initiate all routes
-  Routes(app)
+// Application level error handler
+app.use(errorHandler)
 
-  // Application level error handler
-  app.use(errorHandler)
-  return app
-}
+export { app }
