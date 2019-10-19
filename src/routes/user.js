079b53af890c68c08a805f512b2792103b6913fb
Utkarsh <utkarshdeep@outlok.com>
2019-10-20 02:24:44 +0530

    user route updated

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ import { asyncMiddleware } from '../utils'
 const router = express.Router()
 
 router.post('/signup', asyncMiddleware(UserController.signup))
-router.get('/getUserData', asyncMiddleware(UserController.getUserData))
+router.get('/', asyncMiddleware(UserController.getUserData))
 router.get('/logout', asyncMiddleware(UserController.logout))
 router.post('/login', asyncMiddleware(UserController.login))
 router.post('/updateProfile', asyncMiddleware(UserController.updateProfile))
