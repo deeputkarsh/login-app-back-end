ded04180e3c029a67411eb354e97b60b518408e5
Utkarsh <utkarshdeep@outlok.com>
2019-10-20 17:36:01 +0530

    error handling for jwt errors

--- src/config/error_handler.js
+++ src/config/error_handler.js
@@ -10,6 +10,8 @@ export const errorHandler = (err, req, res, next) => {
 
   if (err.name === 'AppError') return res.status(err.status || 500).json({ error: err.message })
 
+  if (err.name === 'UnauthorizedError') return res.status(httpStatus.UNAUTHORIZED).json({ error: err.message })
+
   // Default handle error
   return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: err.message || 'Something broke !' })
 }
